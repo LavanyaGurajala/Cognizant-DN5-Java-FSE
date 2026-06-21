2aaa57e1fc7edb9ee687beb74092bd8ae5adc23c
LavanyaGurajala <2300033427@kluniversity.in>
2026-06-21 23:28:28 +0530

    week1

---
+++ Week1-DesignPatterns-DSA/DesignPatterns/Exercise2-FactoryMethodPattern/DocumentFactory.java
@@ -0,0 +1,19 @@
+public class DocumentFactory {
+
+    public static Document createDocument(String type) {
+
+        if (type.equalsIgnoreCase("word")) {
+            return new WordDocument();
+        }
+
+        else if (type.equalsIgnoreCase("pdf")) {
+            return new PdfDocument();
+        }
+
+        else if (type.equalsIgnoreCase("excel")) {
+            return new ExcelDocument();
+        }
+
+        return null;
+    }
+}
